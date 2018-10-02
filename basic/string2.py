c712e2df49ee075ad04370538a0e27a59d882eee
Jhong098 <Jhong098@gmail.com>
2018-10-02 18:33:47 -0400

    finished string2

--- basic/string2.py
+++ basic/string2.py
@@ -16,8 +16,13 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
-  # +++your code here+++
-  return
+  if len(s) > 3:
+    if s[-3:] == "ing":
+      return s + "ly"
+    else:
+      return s + "ing"
+  else:
+    return s
 
 
 # E. not_bad
@@ -29,8 +34,12 @@ def verbing(s):
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
 def not_bad(s):
-  # +++your code here+++
-  return
+  found_not = s.find("not")
+  found_bad = s.find("bad")
+  if found_not and found_bad:
+    if found_not < found_bad:
+      return s.replace(s[found_not:found_bad+3], "good")
+  return s
 
 
 # F. front_back
@@ -41,9 +50,16 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-  # +++your code here+++
-  return
+  a_mid = len(a)/2
+  b_mid = len(b)/2
 
+  if len(a) % 2 == 1:
+    a_mid += 1
+  
+  if len(b) % 2 == 1:
+    b_mid += 1
+  
+  return a[:a_mid] + b[:b_mid] + a[a_mid:] + b[b_mid:]
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
