e16034903f2ef870bd82902c6dda3794e1f1c888
Jhong098 <Jhong098@gmail.com>
2018-10-02 02:07:15 -0400

    finished string1

--- basic/string1.py
+++ basic/string1.py
@@ -50,7 +50,7 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  return s[0] + s[1:].replace(s[0], "*")
 
 
 # D. MixUp
@@ -61,8 +61,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  return b[0:2] + a[2:] + " " + a[0:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
